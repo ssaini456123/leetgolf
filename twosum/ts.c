a7bd236a314a757a8513f6f56ad47f1a0ec681fe
Sutinder S. Saini <ssaini456123@gmail.com>
2025-05-19 01:06:17 -0700

    d

--- twosum/ts.c
+++ twosum/ts.c
@@ -1,4 +1,4 @@
-int* twoSum(int*nums,int numsSize,int target,int*returnSize)
+int*twoSum(int*nums,int numsSize,int target,int*returnSize)
 {int a,b,c,d,e,f,x,j,v,w,*z,s;
 a=2,b=pow(10,4);c=pow(-10,9),d=pow(10,9);e=pow(-10,9),f=pow(10,9);z=malloc(8);x=0;s=numsSize;
 while(x<s){if(*(nums+x)>c&&*(nums+x)<d&&(target>e&&target<f)){
