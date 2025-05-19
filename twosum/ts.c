65632dc772a9ee4d6b2d70efdcb803388bbae7a4
Sutinder S. Saini <ssaini456123@gmail.com>
2025-05-19 01:08:48 -0700

    final corrections

--- twosum/ts.c
+++ twosum/ts.c
@@ -1,6 +1,6 @@
-int*twoSum(int*nums,int numsSize,int target,int*returnSize)
-{int a,b,c,d,e,f,x,j,v,w,*z,s;
-a=2,b=pow(10,4);c=pow(-10,9),d=pow(10,9);e=pow(-10,9),f=pow(10,9);z=malloc(8);x=0;s=numsSize;
-while(x<s){if(*(nums+x)>c&&*(nums+x)<d&&(target>e&&target<f)){
-j=x+1;while(j<s){v=*(nums+x);w=*(nums+j);if(v+w==target){z[0]=x,z[1]=j;*returnSize=2;return z;}j++;
+int*twoSum(int*n,int s,int y,int*rs)
+{int a,b,c,d,e,f,x,j,v,w,*z;
+a=2,b=pow(10,4);c=pow(-10,9),d=pow(10,9);e=pow(-10,9),f=pow(10,9);z=malloc(8);x=0;
+while(x<s){if(*(n+x)>c&&*(n+x)<d&&(y>e&&y<f)){
+j=x+1;while(j<s){v=*(n+x);w=*(n+j);if(v+w==y){z[0]=x,z[1]=j;*rs=2;return z;}j++;
 }}x++;}return z;}
